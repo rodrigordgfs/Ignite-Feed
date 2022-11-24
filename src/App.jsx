54d7d06094e02e662572181f467f86d36d46bf2f
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2022-11-24 14:21:25 -0300

    Feature: Concluido a Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import { Header } from './components/Header';
-import './reset.css'
-import './global.css'
+import { Header } from "./components/Header";
+import { SideBar } from "./components/SideBar";
+import styles from "./App.module.css";
+import "./reset.css";
+import "./global.css";
 
 export function App() {
   return (
     <div>
       <Header />
+      <div className={styles.wrapper}>
+        <SideBar />
+        <main>main</main>
+      </div>
     </div>
   );
 }
